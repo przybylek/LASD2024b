20d41cf1f3ad0d123ba24aa5219ed15bf6005578
Adam Przybylek <adam.przybylek@gmail.com>
2024-10-30 22:38:44 +0000

    trademark added

--- sections/Header.js
+++ sections/Header.js
@@ -12,7 +12,7 @@ const Header = () => (
         </div>
         <div className="col-md-6">
           <h1 className='h1 text-center anchor font-weight-bold'>LASD@<a className='u' href='https://isd2024.ug.edu.pl/'>ISD'24</a></h1>
-          <h2 className='text-center h3 py-3'>Track on <strong>Lean and Agile Software Development</strong></h2>
+          <h2 className='text-center h3 py-3'>Track on <strong>Lean and Agile Software Development<sup>&trade;</sup></strong></h2>
           <h3 className='text-center h4 pb-3'><small>at the 32nd International Conference on Information Systems Development</small></h3>
           <h4 className='text-center h5'>August 26 – 28, 2024</h4>
           <h4 className='text-center h5 pb-3'>Sopot, Poland</h4>
